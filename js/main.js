e54008ee01387b3dd0b99c142855518b7c9c9b04
LennoxKyawHtooAung <lkya0002@student.monash.edu>
2026-05-21 19:44:40 +0800

    Publish DV2 transport website

---
+++ js/main.js
@@ -0,0 +1,23 @@
+vegaEmbed("#chart1", "visualisations/chart1_population_heatmap.vg.json", {
+    actions: false
+}).catch(console.error);
+
+vegaEmbed("#chart2", "visualisations/chart2_ranked_district_dotplot.vg.json", {
+    actions: false
+}).catch(console.error);
+
+vegaEmbed("#chart3", "visualisations/chart3_national_ridership_stacked_area.vg.json", {
+    actions: false
+}).catch(console.error);
+
+vegaEmbed("#chart4", "visualisations/chart4_ktmb_small_multiples.vg.json", {
+    actions: false
+}).catch(console.error);
+
+vegaEmbed("#chart5", "visualisations/chart5_ktmb_calendar_heatmap.vg.json", {
+    actions: false
+}).catch(console.error);
+
+vegaEmbed("#chart10", "visualisations/chart10_cyberjaya_bus_stop_map.vg.json", {
+    actions: false
+}).catch(console.error);
